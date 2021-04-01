6644e2a7edbc44fa014d13e2363a4e4f8d0be869
Abhinav Roy <48856484+Drake0306@users.noreply.github.com>
2021-04-01 18:00:01 +0530

    update

--- resources/views/truck_data_scan_pdf.blade.php
+++ resources/views/truck_data_scan_pdf.blade.php
@@ -6,30 +6,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link href="{{url('public/assets/fonts/K010.ttf')}}">
+    <link href="{{url('public/assets/fonts/hindi.css')}}">
+    <link href="{{url('public/assets/fonts/KrutiDev010.woff')}}">
+    
+    <style>
+    @font-face {
+        font-family: 'Kruti Dev 010';
+        src: url('{{url("public/assets/fonts/KrutiDev010.woff2")}}') format('woff2'),
+            url('{{url("public/assets/fonts/KrutiDev010.woff")}}') format('woff');
+        font-weight: normal;
+        font-style: normal;
+        font-display: swap;
+    }
+
+    .KrutiDev_hindi_text {
+        font-family: 'Kruti Dev 010';
+        font-weight: normal;
+        font-style: normal;
+    }
+    </style>
 </head>
 
 <body>
-    <table class="table">
-        <thead>
+    <table width="100%" style="border: 1px solid black" >
             <tr>
-                <th>On</th>
-                <th>alskd</th>
-                <th>asd</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td scope="row"></td>
-                <td></td>
-                <td></td>
+                <th>Image</th>
+                <th>
+                    <u class="KrutiDev_hindi_text" >bafM;u v‚;y d‚j~iksZjs'ku fyfeVsM</u>
+                    <br>
+                    Indian Oil Corporation Limited
+                </th>
+                <th>Space</th>
             </tr>
             <tr>
-                <td scope="row"></td>
-                <td></td>
-                <td></td>
+                <td>sda</td>
+                <td>sa</td>
+                <td>sa</td>
             </tr>
-        </tbody>
+            
     </table>
 </body>
 
